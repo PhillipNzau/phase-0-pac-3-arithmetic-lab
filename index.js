66890ba5699a4f1c5930c2f6085a441acacd8834
phillipnzau <nzauonline78@gmail.com>
2023-03-16 15:18:14 +0300

    Assign Comple

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+function subtract(num1, num2) {
+    return num1 - num2
+}
+function multiply(num1, num2) {
+    return num1 * num2
+}
+function divide(num1, num2) {
+    return num1 / num2
+}
+function increment(n) {
+     n++
+     return n
+}
+
+function decrement(n) {
+     n--
+     return n
+}
+function makeInt(n) {
+    parseInt(n, 10);
+    return n
+}
+function preserveDecimal(n) {
+    parseFloat(n)
+    return n
+}
